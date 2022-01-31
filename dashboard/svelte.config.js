1c01c165fd7cf59ef698808a43c866ebb75196be
agrawalgopala@gmail.com <agrawalgopala@gmail.com>
2022-01-31 18:13:33 +0530

    added dashboard paths

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -11,7 +11,11 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		path: {
+			assets: 'https://svelte-multi-test-dashboard.vercel.app',
+			base: '/dashboard'
+		}
 	}
 };
 
