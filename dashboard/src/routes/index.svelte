02aa267b2dc0c3a6cf41702db3bd2f951fba3307
agrawalgopala@gmail.com <agrawalgopala@gmail.com>
2022-01-31 17:27:39 +0530

    click added to test js resource

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,2 +1,9 @@
+<script>
+	function handleClick() {
+		console.log('Click Handled');
+	}
+</script>
+
 <h1>Welcome to SvelteKit Dashboard</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={handleClick}> click me </button>
