ba325427020fb4495f173f0f2db81c7e5fe7dc07
agrawalgopala@gmail.com <agrawalgopala@gmail.com>
2022-02-01 14:25:44 +0530

    add env variables

---
+++ shell/src/lib/env.ts
@@ -0,0 +1,1 @@
+export const URL_NAME = import.meta.env.VITE_URL_NAME;
--- shell/src/routes/index.svelte
+++ shell/src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script>
+	import { URL_NAME } from '$lib/env';
+
+	console.log(URL_NAME);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
